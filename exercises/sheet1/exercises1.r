69c54b5b22995319b473ff4e94faffa7ce0a4f70
racoelhosilva <racoelhosilva@gmail.com>
2024-02-27 13:36:24 +0000

    fix: restructuring exercises with stripchart and par functions

--- exercises/sheet1/exercises1.r
+++ exercises/sheet1/exercises1.r
@@ -1,8 +1,14 @@
+par(bg = "#282828", col="#ebdbb2", col.axis="#ebdbb2", col.lab="#ebdbb2", col.main="#ebdbb2", fg="#ebdbb2", family="JetBrainsMono Nerd Font")
 file <- read.csv("exercises/sheet1/DadosFolha1.csv", header = TRUE)
 
 ex3 <- function(x = file) {
     data <- na.omit(x$Ex3)
+
+    par(mfrow=c(2,1))
     barplot(table(data), main="Magnitudes de Sismos na Califórnia", xlab="Magnitudes", ylab="Contagem", col="#689d6a")
+    stripchart(data, method="stack", main="Magnitudes de Sismos na Califórnia", xlab="Magnitudes", ylab="Contagem", col="#689d6a", pch=19)
+    par(mfrow=c(1,1))
+
     cat("Mean:", mean(data), "\n")
     cat("Median:", median(data), "\n")
     cat("Standard Deviation:", sd(data), "\n")
@@ -10,8 +16,12 @@ ex3 <- function(x = file) {
 
 ex4 <- function(x = file) {
     data <- na.omit(x$Ex4)
-    # barplot(table(data), main="Perímetro Cefálico de Recém Nascidos do Sexo Masculino", xlab="Perímetro (em cm)", ylab="Contagem", col="#689d6a")
+
+    par(mfrow=c(2,1))
+    barplot(table(data), main="Perímetro Cefálico de Recém Nascidos do Sexo Masculino", xlab="Perímetro (em cm)", ylab="Contagem", col="#689d6a")
     hist(data, main="Perímetro Cefálico de Recém Nascidos do Sexo Masculino", xlab="Perímetro (em cm)", ylab="Frequência", col="#689d6a")
+    par(mfrow=c(1,1))
+
     cat("Mean:", mean(data), "\n")
     cat("Standard Deviation:", sd(data), "\n")
 }
@@ -25,16 +35,24 @@ ex5 <- function(x = file) {
 
 ex6 <- function(x = file) {
     data <- na.omit(x$Ex6)
-    hist(data, main="Atividade de uma enzima nas plaquetas sanguíneas", xlab="Atividade", ylab="Frequência", col="#689d6a")    
+
+    par(mfrow=c(2,1))
+    hist(data, main="Atividade de uma enzima nas plaquetas sanguíneas", xlab="Atividade", ylab="Frequência", col="#689d6a")
+    boxplot(data, main="Atividade de uma enzima nas plaquetas sanguíneas", xlab="Atividade", ylab="Frequência", col="#689d6a", horizontal=TRUE)
+    par(mfrow=c(1,1))
+
     cat("Quartiles:", quantile(data, type=2), "\n")
     cat("Inter-Quartile Range:", IQR(data, type=2), "\n")
 }
 
 ex7 <- function(x = file) {
     data <- na.omit(x$Ex7)
-    # hist(data, main="Comprimentos de tripanossomas", xlab="Comprimento", ylab="Frequência", col="#689d6a", breaks=seq(min(data), max(data), 1)) 
+    
+    par(mfrow=c(2,1))
+    hist(data, main="Comprimentos de tripanossomas", xlab="Comprimento", ylab="Frequência", col="#689d6a", breaks=seq(min(data), max(data), 1)) 
     interval <- ceiling((max(data) - min(data)) / 6)
     hist(data, main="Comprimentos de tripanossomas", xlab="Comprimento", ylab="Frequência", col="#689d6a", breaks=seq(min(data), min(data)+6*interval, interval))
+    par(mfrow=c(1,1))
 }
 
 ex9 <- function(x = file) {
@@ -46,7 +64,12 @@ ex9 <- function(x = file) {
 
 ex11 <- function(x = file) {
     data <- na.omit(x$Ex11)
+
+    par(mfrow=c(2,1))
     barplot(table(data), main="Tamanho da ninhada de porcas", xlab="Tamanho", ylab="Contagem", col="#689d6a")
+    boxplot(data, main="Tamanho da ninhada de porcas", xlab="Tamanho", ylab="Contagem", col="#689d6a", horizontal=TRUE)
+    par(mfrow=c(1,1))
+
     cat("Mean:", mean(data), "\n")
     cat("Median:", median(data), "\n")
     cat("Standard Deviation:", sd(data), "\n")
